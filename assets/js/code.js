2c49f3d2dbe3af7f91b1b58f2f34f421f829fbb1
marcin-kli <klimaszewski.m@gmail.com>
2020-12-27 13:10:05 +0000

    add XMLHttpRequest object in code.js

--- assets/js/code.js
+++ assets/js/code.js
@@ -13,5 +13,21 @@ $(document).ready(function () {
 
     $("#knowledge button").click(function(){
     $("#knowledge .card-body").toggle();
-  })
+    })
 });
+
+    //open USGS API//
+
+    var xhr = new XMLHttpRequest();
+    var url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=1727-11-10&endtime=2020-12-31&minmagnitude=8";
+    xhr.open("GET",url);
+    xhr.send();
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            data = JSON.parse(this.responseText);
+            // cb(data);
+            console.log(data);
+            
+          };
+    }
