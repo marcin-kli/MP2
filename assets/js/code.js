9c125b96e4ad1e14ec6dba0702f897d095ba707a
marcin-kli <klimaszewski.m@gmail.com>
2020-12-27 14:28:26 +0000

    add printGratestData function in code.js

--- assets/js/code.js
+++ assets/js/code.js
@@ -18,16 +18,23 @@ $(document).ready(function () {
 
     //open USGS API//
 
+    var magnitude = 8.4
     var xhr = new XMLHttpRequest();
-    var url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=1727-11-10&endtime=2020-12-31&minmagnitude=8";
-    xhr.open("GET",url);
+    var url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=1727-11-10&endtime=2020-12-31&minmagnitude=";
+    xhr.open("GET",url + magnitude);
     xhr.send();
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-
             data = JSON.parse(this.responseText);
-            // cb(data);
-            console.log(data);
-            
           };
     }
+
+function printGratestData() {
+
+    data=data.features;
+    data.forEach(function(item) { 
+        document.getElementById('greatest-data').innerHTML += "<p>"+ item.properties.mag +"</p>"+
+        "<p>"+ item.properties.place +"</p>"+"<p>"+ item.properties.time +"</p>"+"<p>"+ item.geometry.coordinates +"</p>"
+    });
+    console.log(data.length);
+}
