808966ce7a01065fa2e2c46eadf0283f3c1f1130
marcin-kli <klimaszewski.m@gmail.com>
2020-12-28 11:01:21 +0000

    Add function openUSGSAPI and a table to display a data from API in code.js

--- assets/js/code.js
+++ assets/js/code.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
 });
 
     //open USGS API//
-
+function openUSGSAPI(USGS,callback){
     var magnitude = 8.4
     var xhr = new XMLHttpRequest();
     var url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=1727-11-10&endtime=2020-12-31&minmagnitude=";
@@ -26,15 +26,25 @@ $(document).ready(function () {
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             data = JSON.parse(this.responseText);
+            callback(data);
           };
     }
-
-function printGratestData() {
-
-    data=data.features;
-    data.forEach(function(item) { 
-        document.getElementById('greatest-data').innerHTML += "<p>"+ item.properties.mag +"</p>"+
-        "<p>"+ item.properties.place +"</p>"+"<p>"+ item.properties.time +"</p>"+"<p>"+ item.geometry.coordinates +"</p>"
-    });
+}
+function printGratestData(USGS) {
+    
+    $('#greatest-data').html("")
+   
+    openUSGSAPI(USGS, function(data){
+        data=data.features;
+        data.forEach(function(item) {
+            
+            document.getElementById('greatest-data').innerHTML +=
+            "<table><tr><th>Magnitude</th> <th>Place</th><th>Time</th><th>Coordinates</th></tr><tr><td>"
+            +item.properties.mag+"</td>"+"<td>"+item.properties.place+"</td>"+"<td>"
+            +item.properties.time+"</td>"+"<td>"+item.geometry.coordinates+"</td></tr></table>"
+        });
     console.log(data.length);
+    $('#loading_data').html("")
+
+    })
 }
