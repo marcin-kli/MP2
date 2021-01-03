2f6cae1a1eef4d54eedbd93607ae7b69892f94de
marcin-kli <klimaszewski.m@gmail.com>
2021-01-03 21:10:42 +0000

    Add styling in getmap function, in map.js; Add fixed-top class in navigation, in index.html

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,20 +1,22 @@
 // seperate js file for a map
 // get map API
 var mymap = L.map('mapid', {
-    center: [21.8358, 73.4233],
+    center: [21.8358, 33.4233],
     zoom: 2,
     
     });
 
 L.tileLayer('https://tile.thunderforest.com/atlas/{z}/{x}/{y}{r}.png?apikey=5f689cbf6660484fa5c1a63e197f6d32', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a> , Maps © <a href="http://www.thunderforest.com/">thunderforest</a>',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> , Maps © <a href="http://www.thunderforest.com/">thunderforest</a>',
     maxZoom: 11,
     tileSize: 256,
 }).addTo(mymap);
 
 // add data on the map https://leafletjs.com/reference-1.7.1.html#geojson
 function getmap(){
-
+    $('section').css('margin-top','51px');
+    $('section').css('padding','0');
+    $('section').css('margin-bottom','5.5vh')
 var geoJsonLayer = L.geoJSON(data) .addTo(mymap);
 geoJsonLayer.eachLayer(function(layer) {
    layer.bindPopup("Place: "+layer.feature.properties.place+"<br>Magnitude: "+layer.feature.properties.mag);
